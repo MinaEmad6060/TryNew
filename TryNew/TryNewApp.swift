ead3df7757931affa5429c4d982efd680574fdd5
Mina Emad <minaemad6060@gmail.com>
2025-06-02 16:28:58 +0300

    Initial Commit

---
+++ TryNew/TryNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryNewApp.swift
+//  TryNew
+//
+//  Created by Mina Emad on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TryNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
